23ab0a3afb156da5ca7dabff74b7ebffce6e8520
acing <mailforgames19847@gmail.com>
2024-10-08 22:20:30 +0300

    add wallet connection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,7 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+'use client'
+
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata: Metadata = {
-  title: "Tg web app",
-  description: "ez",
-};
 
 export default function RootLayout({
   children,
@@ -25,10 +10,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <head>
+        <title>TON connect</title>
+      </head>
+      <body>
+        <TonConnectUIProvider manifestUrl="">
+          {children}
+        </TonConnectUIProvider>
       </body>
     </html>
   );
