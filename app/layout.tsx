ab14b78ea06d8170de61534b2f128280ffd35536
acing <mailforgames19847@gmail.com>
2024-10-08 22:31:36 +0300

    add manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
         <title>TON connect</title>
       </head>
       <body>
-        <TonConnectUIProvider manifestUrl="">
+        <TonConnectUIProvider manifestUrl="https://cyan-fantastic-meadowlark-80.mypinata.cloud/ipfs/QmNWpHUjVfu4zw8XYqNsF8oVGqQcQdBH74G1BHBTr8FzLf">
           {children}
         </TonConnectUIProvider>
       </body>
