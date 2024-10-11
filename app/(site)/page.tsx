6de2403ce36071f951d67a76763307e7479dc294
acing <mailforgames19847@gmail.com>
2024-10-11 21:59:32 +0300

    design

---
+++ app/(site)/page.tsx
@@ -0,0 +1,208 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+//
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: any
+    }
+  }
+}
+
+//
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [telegramId, setTelegramId] = useState<string | null>(null);
+  
+  useEffect(() => {
+
+    if (window.Telegram?.WebApp) {
+
+      const initDataString = window.Telegram.WebApp.initData;
+      if (initDataString) {
+        const urlParams = new URLSearchParams(initDataString);
+        try {
+          const user = JSON.parse(urlParams.get('user') || '{}');
+          if (user.id) {
+            setTelegramId(user.id.toString());
+          }
+        } catch (error) {
+          console.error('Error parsing user data:', error);
+        }
+      }
+    }
+  }, []);  
+
+
+  // if (!telegramId) {
+  //   return (
+  //     <main className="flex min-h-screen flex-col items-center justify-center p-4">
+  //       <h1 className="text-4xl font-bold mb-8">Charton dev build</h1>
+  //       <p className="text-xl">This app can only be used within Telegram as a Mini App.</p>
+  //     </main>
+  //   )
+  // }
+
+  // if (isLoading) {
+  //   return (
+  //     <main className="flex min-h-screen flex-col items-center justify-center">
+  //       <div className="bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded">
+  //         Loading...
+  //       </div>
+  //     </main>
+  //   )
+  // }
+
+  return (
+      <div>
+            <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      <div>
+        Main content
+      </div>
+      <div>
+      Main content
+    </div>
+    <div>
+    Main content
+  </div>
+  <div>
+        Main content
+      </div>
+      </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import "./globals.css";
 import { Inter } from 'next/font/google';
 import Script from "next/script";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,13 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>TON connect</title>
+        <title>Charton dev build</title>
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
       </head>
       <body className={inter.className}>
-        <TonConnectUIProvider manifestUrl="https://cyan-fantastic-meadowlark-80.mypinata.cloud/ipfs/QmNWpHUjVfu4zw8XYqNsF8oVGqQcQdBH74G1BHBTr8FzLf">
+        <Header>
           {children}
-        </TonConnectUIProvider>
+        </Header>
       </body>
     </html>
   );
